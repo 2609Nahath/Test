5fc386cc78c94b69b5f87015a20f3b90be5c2407
2609Nahath <nankervis1125@gmail.com>
2024-01-24 13:17:43 +0530

    message

---
+++ lib/CircularImage/CircularImageProfile.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CircularImageProfile extends StatefulWidget {
+  const CircularImageProfile({Key? key}) : super(key: key);
+
+  @override
+  State<CircularImageProfile> createState() => _CircularImageProfileState();
+}
+
+class _CircularImageProfileState extends State<CircularImageProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(5,0,5,0),
+      child: CircleAvatar(
+        radius: 15,
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          radius: 15,
+          child: ClipOval(
+            child: Image.asset(
+              'avatar_1.jpg',
+              height: 30,
+              width: 30,
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+      )
+    );
+  }
+}
--- lib/EmployeeProjectAndTask/Task/AddTask.dart
+++ lib/EmployeeProjectAndTask/Task/AddTask.dart
@@ -16,7 +16,6 @@ class AddTask {
             height: 280,
             width: 500,
             decoration: BoxDecoration(
-              color: Colors.black,
               borderRadius: BorderRadius.circular(20.0), // Adjust the radius as needed
             ),
             child: Scaffold(
@@ -32,40 +31,42 @@ class AddTask {
                       icon: Icon(Icons.arrow_back_ios)
                     )
                   ),
-                  const Align(
-                    alignment: Alignment(-.5,-.5),
+                  Align(
+                    alignment: const Alignment(-.5,-.5),
                     child: TextField(
                         decoration: InputDecoration(
                         hintText: 'Add Task', // Hint text
-                        border: OutlineInputBorder(), // Add border for better visibility
+                        border: const OutlineInputBorder(),
+                        hintStyle: Fonts.hintFonts // Add border for better visibility
                       ),
                     ),
                   ),
-                  const Align(
-                    alignment: Alignment(.5, .5),
+                  Align(
+                    alignment: const Alignment(.5, .5),
                     child: TextField(
                       maxLines: 3,
                         decoration: InputDecoration(
                         hintText: 'Notes', // Hint text
-                        border: OutlineInputBorder(), // Add border for better visibility
+                        border: const OutlineInputBorder(),
+                        hintStyle: Fonts.hintFonts // Add border for better visibility
                       ),
                     ),
                   ),             
                   Align(
                     alignment: const Alignment(0,1),
                     child: ElevatedButton(
-                      onPressed: () => {}, 
-                      child: Text("Insert", style: Fonts.bodyFonts),
+                      onPressed: () => {},
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 1, 189, 178)),
-                      ),
+                      ), 
+                      child: Text("Add Task", style: Fonts.bodyFonts),
                     )
                   ),
                   Align(
                     alignment: Alignment.topRight,
                     child: IconButton(
                       onPressed: () => {}, // Refer step 3
-                      icon: Icon(Icons.calendar_month_outlined),
+                      icon: const Icon(Icons.calendar_month_outlined),
                     ),
                   )
                 ],
